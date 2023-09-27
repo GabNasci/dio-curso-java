da14ff204c5afbb2d91d70a269b1130d218af158
Gabriel Nascimento <gabriel.nascimento@grupointegrado.br>
2023-09-27 17:47:57 -0300

    Tipos

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,17 @@
 public class TiposVariaveis {
+
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+
+        final double VALOR_DE_PI = 3.14;
+
+        String nomeCompleto = "Gabriel" + " " + "Nascimento";
+        System.out.println(nomeCompleto);
     }
 }
